336ea630e85fa905903c780dc96c06a4d6b5d78f
Shubham Parmar <pshubham@gkblabs.com>
2021-05-21 13:17:51 +0530

    created fileds with all types

--- resources/views/contacts/create.blade.php
+++ resources/views/contacts/create.blade.php
@@ -34,14 +34,30 @@
               <label for="city">City:</label>
               <input type="text" class="form-control" name="city"/>
           </div>
-          <div class="form-group">
-              <label for="country">Country:</label>
-              <input type="text" class="form-control" name="country"/>
+          <label for="country">Country:</label>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" checked name="country" id="India" value="India">
+            <label class="form-check-label" for="India">India</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" name="country" id="United State" value="United State">
+            <label class="form-check-label" for="United State">United State</label>
           </div>
           <div class="form-group">
-              <label for="job_title">Job Title:</label>
-              <input type="text" class="form-control" name="job_title"/>
-          </div>                         
+            <label for="job_title">Job Title:</label>
+            <select class="form-control" id="job_title" name="job_title">
+              <option disabled selected value="">Select</option>
+              <option value="Team Leader">Team Leader</option>
+              <option value="Manager">Manager</option>
+              <option value="Assistant Manager">Assistant Manager</option>
+              <option value="Executive">Executive</option>
+              <option value="Director">Director</option>
+              <option value="Coordinator">Coordinator</option>
+              <option value="Administrator">Administrator</option>
+              <option value="Controller">Controller</option>
+              <option value="Project Manager">Project Manager</option>
+            </select>
+          </div>                    
           <button type="submit" class="btn btn-primary-outline">Add contact</button>
       </form>
   </div>
--- resources/views/contacts/edit.blade.php
+++ resources/views/contacts/edit.blade.php
@@ -36,13 +36,29 @@
                 <label for="city">City:</label>
                 <input type="text" class="form-control" name="city" value={{ $contact->city }} />
             </div>
-            <div class="form-group">
-                <label for="country">Country:</label>
-                <input type="text" class="form-control" name="country" value={{ $contact->country }} />
+            <label for="country">Country:</label>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" <?php if($contact->country == 'India'){ echo 'checked'; } ?> name="country" id="India" value="India">
+                <label class="form-check-label" for="India">India</label>
+            </div>
+            <div class="form-check form-check-inline">
+                <input class="form-check-input" type="radio" <?php if($contact->country == 'United State'){ echo 'checked'; } ?> name="country" id="United State" value="United State">
+                <label class="form-check-label" for="United State">United State</label>
             </div>
             <div class="form-group">
                 <label for="job_title">Job Title:</label>
-                <input type="text" class="form-control" name="job_title" value={{ $contact->job_title }} />
+                <select class="form-control" id="job_title" name="job_title">
+                <option disabled selected value="">Select</option>
+                <option value="Team Leader" <?php if($contact->job_title == 'Team Leader'){ echo "selected"; }?>>Team Leader</option>
+                <option value="Manager" <?php if($contact->job_title == 'Manager'){ echo "selected"; }?>>Manager</option>
+                <option value="Assistant Manager" <?php if($contact->job_title == 'Assistant Manager'){ echo "selected"; }?>>Assistant Manager</option>
+                <option value="Executive" <?php if($contact->job_title == 'Executive'){ echo "selected"; }?>>Executive</option>
+                <option value="Director" <?php if($contact->job_title == 'Director'){ echo "selected"; }?>>Director</option>
+                <option value="Coordinator" <?php if($contact->job_title == 'Coordinator'){ echo "selected"; }?>>Coordinator</option>
+                <option value="Administrator" <?php if($contact->job_title == 'Administrator'){ echo "selected"; }?>>Administrator</option>
+                <option value="Controller" <?php if($contact->job_title == 'Controller'){ echo "selected"; }?>>Controller</option>
+                <option value="Project Manager" <?php if($contact->job_title == 'Project Manager'){ echo "selected"; }?>>Project Manager</option>
+                </select>
             </div>
             <button type="submit" class="btn btn-primary">Update</button>
         </form>
